068aa5310d97c834e029b14424ec37797927c80e
Stefania <stefania.kovtun@yandex.ru>
2024-06-27 21:53:50 +0300

    Доп измене

--- dul.py
+++ dul.py
@@ -20,7 +20,7 @@ class Menu:
 
     def appendDish(self, line):
         """Добавляет блюдо в меню без записи в файл."""
-        parts = line.strip().split(';')
+        parts = line.strip().split('&')
         if len(parts) == 4:
             name, category, price, weight = parts
             try:
@@ -33,14 +33,38 @@ class Menu:
         else:
             print(f"Некорректная строка: {line.strip()}")
 
-             
+    def deleteDish(self, name):
+        """Удаляет блюдо из меню."""
+        if name in self.menu_dict:
+            del self.menu_dict[name]
+            self.count -= 1
+            print(f"Блюдо '{name}' удалено.")
+        else:
+            print(f"Блюда '{name}' нет в меню.")
+
+    def updateDish(self, old_name, new_name, new_category, new_price, new_weight):
+        """Изменяет данные существующего блюда в меню."""
+        if old_name in self.menu_dict:
+            dish = self.menu_dict.pop(old_name)  # Удаляем старую запись
+            dish.name = new_name
+            dish.category = new_category
+            dish.price = int(new_price)
+            dish.weight = int(new_weight)
+            self.menu_dict[new_name] = dish   # Добавляем обновленную запись
+            print(f"Блюдо '{old_name}' обновлено.")
+            return True
+        else:
+            print(f"Блюда '{old_name}' нет в меню.")
+            return False
+
+                  
     def read_menu_from_file(self, filename):
         """Считывает данные о блюдах из файла."""
         menu_dict = {}
         try:
             with open(filename, 'r', encoding='utf-8') as file:
                 for line in file:
-                    parts = line.strip().split(';')
+                    parts = line.strip().split('&')
                     if len(parts) == 4:
                         name, category, price, weight = parts
                         try:
@@ -54,5 +78,6 @@ class Menu:
         except FileNotFoundError:
             print(f"Файл '{filename}' не найден.")
         return menu_dict
+
 
 "# Bludo" 
